bc95bee81da23d93f5d2de6ba3988664aae1e374
rakshi20 <rakshith.c.acharya@gmail.com>
2024-05-13 17:22:10 +0530

    database connection info updated for railway.app deployment

--- src/main/java/DatabaseConnector.java
+++ src/main/java/DatabaseConnector.java
@@ -8,11 +8,17 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class DatabaseConnector implements ServletContextListener {
+
+    private final String host = System.getProperty("PGHOST");
+    private final String port = System.getProperty("PGPORT");
+    private final String userName = System.getProperty("POSTGRES_USER");
+    private final String name = System.getProperty("PGDATABASE");
+    private final String password = System.getProperty("POSTGRES_PASSWORD");
     public void contextInitialized(ServletContextEvent event) {
         try {
-            String url = "jdbc:postgresql://localhost:5432/QnA";
-            String userName = "admin";
-            String password = "password";
+            String url = "jdbc:postgresql://" + host+ ":" + port + "/" + name;
+            String userName = this.userName;
+            String password = this.password;
 
             Class.forName("org.postgresql.Driver");
 
