994b09b33c3cc54124a78ff44953090f1423beac
rakshi20 <rakshith.c.acharya@gmail.com>
2024-05-15 23:44:55 +0530

    added config.yml using snakeyaml

--- src/main/java/DatabaseConnector.java
+++ src/main/java/DatabaseConnector.java
@@ -1,22 +1,32 @@
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
+import org.yaml.snakeyaml.Yaml;
 
+import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DatabaseConnector implements ServletContextListener {
 
-    private final String host = "localhost";
-    private final String port = "5432";
-    private final String userName = "admin";
-    private final String name = "QnA";
-    private final String password = "password";
+    private String host, userName, dbname, password;;
+    private Integer port;
     public void contextInitialized(ServletContextEvent event) {
+        Yaml yaml = new Yaml();
         try {
-            String url = "jdbc:postgresql://" + host+ ":" + port + "/" + name;
+            HashMap mp = yaml.loadAs(new FileReader("D:\\Intellij Projects\\QnA\\src\\main\\resources\\config.yaml"), Map.class);
+            setDBdata(mp);
+        }
+        catch (Exception e) {
+            System.out.println("Config issue.");
+            e.printStackTrace();
+        }
+        try {
+            String url = "jdbc:postgresql://" + host+ ":" + port + "/" + dbname;
             String userName = this.userName;
             String password = this.password;
 
@@ -27,11 +37,21 @@ public class DatabaseConnector implements ServletContextListener {
             ServletContext servletContext = event.getServletContext();
             servletContext.setAttribute("dbCon", connection);
         } catch (Exception e) {
+            System.out.println("Database connection issue.");
             e.printStackTrace();
         }
     }
 
     public void contextDestroyed(ServletContextEvent event) {
         System.out.println("Project undeployed!");
+    }
+
+    private void setDBdata(HashMap mp) {
+        this.host = (String)mp.get("host");
+        this.port = (Integer)mp.get("port");
+        this.dbname = (String)mp.get("dbname");
+        this.userName = (String) mp.get("userName");
+        this.password = (String) mp.get("password");
+        //System.out.println(host + " " + port + " " + dbname + " " + userName + " " + password);
     }
 }
